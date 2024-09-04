cc1bf44abff6811f78d905f6a47a6d33c0550afc
gyeol <gyeol9012@gmail.com>
2024-09-04 14:02:35 +0900

    [UPDATE] 회원 삭제 구현

--- src/main/java/com/sparta/newsfeed19/user/UserService.java
+++ src/main/java/com/sparta/newsfeed19/user/UserService.java
@@ -1,17 +1,14 @@
 package com.sparta.newsfeed19.user;
 
-import com.sparta.newsfeed19.user.dto.SimpleResponseDto;
 import com.sparta.newsfeed19.global.config.PasswordEncoder;
 import com.sparta.newsfeed19.global.exception.ApiException;
 import com.sparta.newsfeed19.global.exception.ResponseCode;
 import com.sparta.newsfeed19.global.util.JwtUtil;
-import com.sparta.newsfeed19.user.dto.LoginRequestDto;
-import com.sparta.newsfeed19.user.dto.SaveUserRequestDto;
-import com.sparta.newsfeed19.user.dto.SaveUserResponseDto;
 import com.sparta.newsfeed19.user.dto.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import static com.sparta.newsfeed19.global.exception.ResponseCode.*;
 
 
@@ -23,6 +20,7 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
 
+    // 유저 등록
     @Transactional
     public SaveUserResponseDto saveUser(SaveUserRequestDto saveUserRequestDto) {
 
@@ -44,7 +42,7 @@ public class UserService {
         );
     }
 
-    //유저 로그인
+    // 유저 로그인
     @Transactional
     public String login(LoginRequestDto requestDto) {
         String email = requestDto.getEmail();
@@ -53,27 +51,42 @@ public class UserService {
 
         // 사용자 확인
         User user = userRepository.findByEmail(email).orElseThrow(
-                ()->new IllegalArgumentException("등록된 회원이 없습니다.")
+                () -> new IllegalArgumentException("등록된 회원이 없습니다.")
         );
 
         // 비밀번호 확인 인코딩 버전
-        if (!passwordEncoder.matches(password,user.getPassword())) {
-                throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
+
+        // 탈퇴한 회원이 아닌지 확인
+        boolean isActiveUser = user.getDeletedAt() == null;
+        if (!isActiveUser) {
+            throw new ApiException(ResponseCode.NOT_FOUND_USER);
+        }
+
 
         // JWT 생성 및 쿠키에 저장 후 Response 에 추가
         String token = jwtUtil.createToken(user.getEmail());
 
+
         return token;
     }
 
-    //유저 조회
-    public SimpleResponseDto getUser(Long id) {
+    // 유저 조회
+    public GetUserResponseDto getUser(Long id) {
         User user = userRepository.findById(id)
-                .orElseThrow(()-> new ApiException(ResponseCode.NOT_FOUND_USER));
-        return new SimpleResponseDto(user);
+                .orElseThrow(() -> new ApiException(ResponseCode.NOT_FOUND_USER));
+
+        // 탈퇴한 회원이 아닌지 확인
+        boolean isActiveUser = user.getDeletedAt() == null;
+        if (!isActiveUser) {
+            throw new ApiException(ResponseCode.NOT_FOUND_USER);
+        }
+        return new GetUserResponseDto(user);
     }
 
+    // 유저 수정
     @Transactional
     public void updateUserPassword(
             Long userId,
@@ -91,6 +104,7 @@ public class UserService {
         }
 
         user.updatePassword(passwordEncoder.encode(updateUserRequestDto.getNewPassword()));
+        userRepository.save(user);
     }
 
     // 유저 삭제
