17fe4ba8bbecb91bfd27966983fd77d8c1dd7329
kim-na-ram <snrneh3@naver.com>
2024-09-04 14:35:28 +0900

    [TEST] Follow 테스트 코드 구현중

---
+++ src/test/java/com/sparta/newsfeed19/follow/FollowControllerTest.java
@@ -0,0 +1,255 @@
+package com.sparta.newsfeed19.follow;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.sparta.newsfeed19.follow.dto.FollowRequestDto;
+import com.sparta.newsfeed19.follow.dto.FollowerResponseDto;
+import com.sparta.newsfeed19.follow.dto.FollowingResponseDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = FollowController.class)
+@ExtendWith(SpringExtension.class)
+class FollowControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private FollowService followService;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    public static String asJsonString(final Object obj) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+            return mapper.registerModule(new JavaTimeModule()).writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @BeforeEach
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .addFilters(new CharacterEncodingFilter("UTF-8", true))  // 필터 추가
+                .alwaysDo(print())
+                .build();
+    }
+
+    @Test
+    @DisplayName("팔로우(v1)에 성공한다.")
+    public void follow_v1_success() throws Exception {
+        // given
+        FollowRequestDto followRequestDto = new FollowRequestDto(1L, "test@test.com");
+
+        willDoNothing().given(followService).follow(followRequestDto.getEmail(), followRequestDto);
+
+        // when, then
+        mockMvc.perform(post("/api/v1/following")
+                        .content(asJsonString(followRequestDto))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("정상 처리되었습니다"));
+    }
+
+    @Test
+    @DisplayName("언팔로우(v1)에 성공한다.")
+    public void unfollow_v1_success() throws Exception {
+        // given
+        FollowRequestDto followRequestDto = new FollowRequestDto(1L, "test@test.com");
+
+        willDoNothing().given(followService).unfollow(followRequestDto.getEmail(), followRequestDto);
+
+        // when, then
+        mockMvc.perform(delete("/api/v1/following")
+                        .content(asJsonString(followRequestDto))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("정상 처리되었습니다"));
+    }
+
+    @Test
+    @DisplayName("팔로워 목록 조회(v1)에 성공한다.")
+    public void getFollowerList_v1_success() throws Exception {
+        // given
+        FollowRequestDto followRequestDto = new FollowRequestDto(null, "test@test.com");
+
+        FollowerResponseDto followerResponseDto1 = new FollowerResponseDto(1L);
+        FollowerResponseDto followerResponseDto2 = new FollowerResponseDto(2L);
+        FollowerResponseDto followerResponseDto3 = new FollowerResponseDto(3L);
+        List<FollowerResponseDto> followerResponseDtoList =
+                List.of(followerResponseDto1, followerResponseDto2, followerResponseDto3);
+
+        given(followService.getFollowerList(anyString())).willReturn(followerResponseDtoList);
+
+        // when, then
+        mockMvc.perform(get("/api/v1/followers")
+                        .content(asJsonString(followRequestDto))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").isArray())
+                .andExpect(jsonPath("$..['followerId']").exists());
+    }
+
+    @Test
+    @DisplayName("팔로잉 목록 조회(v1)에 성공한다.")
+    public void getFollowingList_v1_success() throws Exception {
+        // given
+        FollowRequestDto followRequestDto = new FollowRequestDto(null, "test@test.com");
+
+        FollowingResponseDto followingResponseDto1 = new FollowingResponseDto(1L);
+        FollowingResponseDto followingResponseDto2 = new FollowingResponseDto(2L);
+        FollowingResponseDto followingResponseDto3 = new FollowingResponseDto(3L);
+        List<FollowingResponseDto> followingResponseDtoList =
+                List.of(followingResponseDto1, followingResponseDto2, followingResponseDto3);
+
+        given(followService.getFollowingList(anyString())).willReturn(followingResponseDtoList);
+
+        // when, then
+        mockMvc.perform(get("/api/v1/following")
+                        .content(asJsonString(followRequestDto))
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").isArray())
+                .andExpect(jsonPath("$..['followingId']").exists());
+    }
+
+    /** 최종 API 테스트 **/
+
+    @Test
+    @DisplayName("팔로우에 성공한다.")
+    public void follow_success() throws Exception {
+        // given
+        String email = "test@test.com";
+        FollowRequestDto followRequestDto = new FollowRequestDto(1L, null);
+
+        willDoNothing().given(followService).follow(email, followRequestDto);
+
+        // when, then
+        mockMvc.perform(post("/api/following")
+                        .content(asJsonString(followRequestDto))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("정상 처리되었습니다"));
+    }
+
+    @Test
+    @DisplayName("팔로우할 유저가 명시되지 않아 팔로우에 실패한다.")
+    public void follow_validation_failure() throws Exception {
+        // given
+        String email = "test@test.com";
+        FollowRequestDto followRequestDto = new FollowRequestDto(null, null);
+
+        willDoNothing().given(followService).follow(email, followRequestDto);
+
+        // when, then
+        mockMvc.perform(post("/api/following")
+                        .content(asJsonString(followRequestDto))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("(언)팔로우 할 유저의 고유번호는 필수입니다."));
+    }
+
+    @Test
+    @DisplayName("언팔로우에 성공한다.")
+    public void unfollow_success() throws Exception {
+        // given
+        FollowRequestDto followRequestDto = new FollowRequestDto(1L, null);
+
+        willDoNothing().given(followService).unfollow(followRequestDto.getEmail(), followRequestDto);
+
+        // when, then
+        mockMvc.perform(delete("/api/following")
+                        .content(asJsonString(followRequestDto))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("정상 처리되었습니다"));
+    }
+
+    @Test
+    @DisplayName("언팔로우할 유저가 명시되지 않아 언팔로우에 실패한다.")
+    public void unfollow_validation_failure() throws Exception {
+        // given
+        String email = "test@test.com";
+        FollowRequestDto followRequestDto = new FollowRequestDto(null, null);
+
+        willDoNothing().given(followService).follow(email, followRequestDto);
+
+        // when, then
+        mockMvc.perform(delete("/api/following")
+                        .content(asJsonString(followRequestDto))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("(언)팔로우 할 유저의 고유번호는 필수입니다."));
+    }
+
+    @Test
+    @DisplayName("팔로워 목록 조회에 성공한다.")
+    public void getFollowerList_success() throws Exception {
+        // given
+        FollowerResponseDto followerResponseDto1 = new FollowerResponseDto(1L);
+        FollowerResponseDto followerResponseDto2 = new FollowerResponseDto(2L);
+        FollowerResponseDto followerResponseDto3 = new FollowerResponseDto(3L);
+        List<FollowerResponseDto> followerResponseDtoList =
+                List.of(followerResponseDto1, followerResponseDto2, followerResponseDto3);
+
+        given(followService.getFollowerList(anyString())).willReturn(followerResponseDtoList);
+
+        // when, then
+        mockMvc.perform(get("/api/followers"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").isArray())
+                .andExpect(jsonPath("$..['followerId']").exists());
+    }
+
+    @Test
+    @DisplayName("팔로잉 목록 조회에 성공한다.")
+    public void getFollowingList_success() throws Exception {
+        // given
+        FollowingResponseDto followingResponseDto1 = new FollowingResponseDto(1L);
+        FollowingResponseDto followingResponseDto2 = new FollowingResponseDto(2L);
+        FollowingResponseDto followingResponseDto3 = new FollowingResponseDto(3L);
+        List<FollowingResponseDto> followingResponseDtoList =
+                List.of(followingResponseDto1, followingResponseDto2, followingResponseDto3);
+
+        given(followService.getFollowingList(anyString())).willReturn(followingResponseDtoList);
+
+        // when, then
+        mockMvc.perform(get("/api/following"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data").isArray())
+                .andExpect(jsonPath("$..['followingId']").exists());
+    }
+}
