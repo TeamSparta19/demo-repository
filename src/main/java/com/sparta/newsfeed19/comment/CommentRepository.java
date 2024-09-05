7e2011140c388635357f1c4fa315615bcbaeb1eb
Choi1999 <dprtldk157@gmail.com>
2024-09-05 19:28:48 +0900

    FEAT log 수정

--- src/main/java/com/sparta/newsfeed19/comment/CommentRepository.java
+++ src/main/java/com/sparta/newsfeed19/comment/CommentRepository.java
@@ -4,7 +4,6 @@ package com.sparta.newsfeed19.comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
