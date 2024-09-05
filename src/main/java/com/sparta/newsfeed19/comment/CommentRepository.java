25c3a8553972a8cf160e3ec2f69326df04a7cce5
Choi1999 <dprtldk157@gmail.com>
2024-09-05 19:28:08 +0900

    FEAT log 수정

--- src/main/java/com/sparta/newsfeed19/comment/CommentRepository.java
+++ src/main/java/com/sparta/newsfeed19/comment/CommentRepository.java
@@ -8,9 +8,5 @@ import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findAllByPostIdAndDeletedAtIsNull(Long postId);  // 특정 게시물의 삭제되지 않은 모든 댓글 조회
 
-    List<Comment> findAllByUserEmailAndDeletedAtIsNull(String email);  // 특정 사용자의 삭제되지 않은 모든 댓글 조회
-
-    List<Comment> findAllByPostIdAndUserEmailAndDeletedAtIsNull(Long postId, String email);  // 특정 게시물의 특정 사용자가 작성한 삭제되지 않은 댓글 조회
 }
